a76853327baa9c816a61db7639bf0dd6c89a9cea
artfalcao <arthurmonteirofalcao1995@gmail.com>
2024-05-14 18:22:48 -0300

    feat: form validation

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -2,64 +2,107 @@ import Link from "next/link";
 import { useRouter } from 'next/router'
 import { FormEvent, useState } from "react";
 import { logIn } from "@/services/auth/auth.service";
+import {
+  Formik,
+  Form,
+  Field,
+} from 'formik';
+import * as Yup from 'yup';
+
+const signInSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, 'Too Short!')
+    .max(30, 'Too Long!')
+    .required('Required'),
+  password: Yup.string()
+    .min(6, 'Too Short!')
+    .max(12, 'Too Long!')
+    .required('Required'),
+});
+
+interface MySignInProps {
+  username: string,
+  password: string
+}
+
+const initialValues: MySignInProps = {
+  username: '',
+  password: '',
+}
 
 const LoginForm = () => {
-  const router = useRouter()
+  const router = useRouter();
 
-  const [username, setUSerName] = useState('');
-  const [password, setPassword] = useState('');
   const [errorMsg, setErrorMsg] = useState('');
   const [alert, setAlert] = useState(false);
 
-  const handleLogin = async(e:FormEvent) => {
-    e.preventDefault();
-    const user = await logIn({ username, password });
+  const handleLogin = async (values: MySignInProps) => {
+    const user = await logIn(values);
     if (user) {
       router.push('/home');
     }
 
     setErrorMsg('User not found! Please try again.');
-    // TO DO: turn alert to TRUE and show alert component with error message
+    setAlert(true);
+    // TO DO: show alert component with error message
   };
 
   return (
     <>
       <div className={`mt-12`}>
-        <form className={`flex flex-col gap-4`} onSubmit={handleLogin}>
-          <input
-            name="username"
-            placeholder="Username"
-            className={`py-2 pl-2 rounded-lg bg-neutral-200 placeholder:text-gray-500`}
-            value={ username }
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUSerName(e.target.value)}
-          ></input>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={signInSchema}
+          onSubmit={async (values, actions) => {
+            await handleLogin(values);
+            actions.setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting, errors, touched }) => (
+            <Form className={`flex flex-col gap-4`}>
+              <label htmlFor="username" className={`block text-md font-medium leading-6 text-gray-900`}>Username</label>
+              <Field
+                className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-500 sm:text-sm sm:leading-6`}
+                name="username"
+              />
+              {errors.username && touched.username ? (
+                <div className="text-red-500">{errors.username}</div>
+              ) : null}
 
-          <input
-            name="password"
-            placeholder="Password"
-            className={`py-2 pl-2 rounded-lg bg-neutral-200 placeholder:text-gray-500`}
-            value={ password }
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-          ></input>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className={`block text-md font-medium leading-6 text-gray-900`}>Password</label>
+                <div className="text-sm">
+                  <Link
+                    href='/forgotpassword'
+                    className={`font-semibold text-stone-900 hover:text-stone-700`}
+                  >Forgot Password?</Link>
+                </div>
+              </div>
+              <Field
+                className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-500 sm:text-sm sm:leading-6`}
+                name="password"
+              />
+              {errors.password && touched.password ? (
+                <div className="text-red-500">{errors.password}</div>
+              ) : null}
+              
+              <button
+                className={`mt-6 bg-yellow-400 p-2 rounded-lg font-semibold text-secondary text-md shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-500`}
+                type="submit"
+                disabled={isSubmitting}
+              >{isSubmitting ? 'Signin...' : 'Signin'}
+              </button>
+            </Form>
+          )}
+        </Formik>
 
-          <div className={`flex justify-end mt-2`}>
-            <Link
-              href='/forgot-password'
-              className={`font-semibold text-neutral-600 underline`}
-            >Forgot Password?</Link>
-          </div>
-
-          <button className={`mt-4 bg-primary p-2 rounded-lg font-semibold text-secondary text-md hover:bg-secondary hover:text-primary transition ease-in-out delay-150`}
-          >Sign In</button>
-        </form>
-      </div>
-
-      <div className={`flex gap-2 mt-4`}>
-        <p className={`text-neutral-600`}>Don't have an account?</p>
-        <Link
-          href='/sign-up'
-          className={`text-secondary font-bold`}
-        >Sign Up now</Link>
+        <div className={`flex gap-2 mt-4`}>
+          <p className={`text-neutral-600`}>Don't have an account?</p>
+          <Link
+            href='/sign-up'
+            className={`text-secondary font-bold`}
+          >Sign Up now</Link>
+        </div>
       </div>
     </>
   );
