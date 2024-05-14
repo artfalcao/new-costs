ea50e62e9c8b68d3e45d9a276d3dd361f73d3eb8
artfalcao <arthurmonteirofalcao1995@gmail.com>
2024-05-14 18:23:18 -0300

    styles: change position and btn-color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,10 @@ const Login = () => {
         <img src='./home_img.svg' alt='Pig and coin' width={500} height={500}/>
       </section>
 
-      <section className={`w-9/12 mx-auto pt-8 flex flex-col gap-2`}>
+      <section className={`w-9/12 mx-auto pt-8 flex flex-col gap-2 justify-center`}>
         <div className={`flex flex-nowrap gap-2 justify-center items-center`}>
           <h1 className={`min-w-max text-5xl font-medium`}>Welcome to</h1>
-          <span className="max-h-min font-bold uppercase text-6xl text-primary bg-secondary rounded-lg p-1">Costs!</span>
+          <span className="max-h-min font-bold uppercase text-6xl text-yellow-500 bg-secondary rounded-lg p-1">Costs!</span>
         </div>
 
         <LoginForm />
